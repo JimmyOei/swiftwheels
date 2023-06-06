029a4ed51f4611863ae08e3febac46f2e3f206ae
Jimmy Oei <Jimmy.oei10@gmail.com>
2023-06-06 21:29:43 +0200

    project start

---
+++ src/main/java/com/jimmyoei/swiftwheels/SwiftwheelsApplication.java
@@ -0,0 +1,13 @@
+package com.jimmyoei.swiftwheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftwheelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftwheelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jimmyoei/swiftwheels/SwiftwheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimmyoei.swiftwheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftwheelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
